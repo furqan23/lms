a29fcc7dd0ce9174e0146b02a2a75e5c9c2964ae
Furqan <engr.furqan1@gmail.com>
2023-09-22 18:03:25 +0500

    test

--- lib/view/my_test/gettest.dart
+++ lib/view/my_test/gettest.dart
@@ -3,7 +3,6 @@ import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:splashapp/model/get_test_model.dart';
-import 'package:splashapp/test.dart';
 import 'package:splashapp/view/quizz/get_testquestion_view.dart';
 import 'package:splashapp/widget/testcard_widget.dart';
 
@@ -54,7 +53,7 @@ class _GetTestState extends State<GetTest> {
                 return InkWell(
 
                   onTap: () {
-                    Get.to(()=>DemoApp());
+                    // Get.to(()=>DemoApp());
                    // Get.to(()=>QuizzView(id: getTestList[0].data![index].id.toString()));
                   },
                   child: TestCard(
