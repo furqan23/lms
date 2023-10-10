12f2c34169deb97ecdc3d6f9fc4d6f3d758c656c
Khushalkhankhattak11 <khushiikhan1125@gmail.com>
2023-10-10 11:12:55 +0500

    10/10/2023 11:12

--- lib/values/auth_api.dart
+++ lib/values/auth_api.dart
@@ -1,46 +1,46 @@
 class AuthApi {
   static const String _name = "name";
 
-  static const String loginApi = "https://dav.binshaharts.com/api/v1/login";
+  static const String loginApi = "https://cmi.edu.pk/qca/api/v1/login";
   static const String registerApi =
-      "https://dav.binshaharts.com/api/v1/register";
+      "https://cmi.edu.pk/qca/api/v1/register";
   static const String resetpasswordApi =
-      "https://dav.binshaharts.com/api/v1/password/email";
+      "https://cmi.edu.pk/qca/api/v1/password/email";
   static const String otpApi =
-      "https://dav.binshaharts.com/api/v1/password/code/check";
+      "https://cmi.edu.pk/qca/api/v1/password/code/check";
   static const String resetspasswordApi =
-      "https://dav.binshaharts.com/api/v1/password/reset";
+      "https://cmi.edu.pk/qca/api/v1/password/reset";
   static const String getDashboardApi =
-      "https://dav.binshaharts.com/api/v1/dashboard";
+      "https://cmi.edu.pk/qca/api/v1/dashboard";
   static const String courseApi =
-      "https://dav.binshaharts.com/api/v1/courseList";
+      "https://cmi.edu.pk/qca/api/v1/courseList";
   static const String getPayApi =
-      "https://dav.binshaharts.com/api/v1/student/myPayments";
+      "https://cmi.edu.pk/qca/api/v1/student/myPayments";
   static const String postInvoiceById =
-      "https://dav.binshaharts.com/api/v1/student/getInvoiceById";
+      "https://cmi.edu.pk/qca/api/v1/student/getInvoiceById";
   static const String getstudentCourse =
-      "https://dav.binshaharts.com/api/v1/student/myCourses";
+      "https://cmi.edu.pk/qca/api/v1/student/myCourses";
   static const String getstudentCourseById =
-      "https://dav.binshaharts.com/api/v1/student/getCourseById";
+      "https://cmi.edu.pk/qca/api/v1/student/getCourseById";
   static const String uploadReceiptApi =
-      "https://dav.binshaharts.com/api/v1/student/uploadReceipt";
+      "https://cmi.edu.pk/qca/api/v1/student/uploadReceipt";
   static const String videoLecturesApi =
-      "https://dav.binshaharts.com/api/v1/student/getCourseVideos";
+      "https://cmi.edu.pk/qca/api/v1/student/getCourseVideos";
   static const String getTestApi =
-      "https://dav.binshaharts.com/api/v1/student/getCourseTests";
+      "https://cmi.edu.pk/qca/api/v1/student/getCourseTests";
   static const String startTestApi =
-      "https://dav.binshaharts.com/api/v1/student/startTest";
+      "https://cmi.edu.pk/qca/api/v1/student/startTest";
 
-  static const String testinfo ="https://dav.binshaharts.com/api/v1/student/testInfo";
+  static const String testinfo ="https://cmi.edu.pk/qca/api/v1/student/testInfo";
   static const String getQuestionTestApi =
-      "https://dav.binshaharts.com/api/v1/student/getTestQuestions";
+      "https://cmi.edu.pk/qca/api/v1/student/getTestQuestions";
   static const String postAnswerApi =
-      "https://dav.binshaharts.com/api/v1/student/postAnswers";
-  static const String getMyResult1 ="https://dav.binshaharts.com/api/v1/student/myResultsCourses";
+      "https://cmi.edu.pk/qca/api/v1/student/postAnswers";
+  static const String getMyResult1 ="https://cmi.edu.pk/qca/api/v1/student/myResultsCourses";
   static const String getMyResultsTestApi =
-      "https://dav.binshaharts.com/api/v1/student/myResultsTests";
+      "https://cmi.edu.pk/qca/api/v1/student/myResultsTests";
   static const String getMyfinalResultApi =
-      "https://dav.binshaharts.com/api/v1/student/myFinalResults";
+      "https://cmi.edu.pk/qca/api/v1/student/myFinalResults";
 
-  static const String baseUrlSliderImage="https://dav.binshaharts.com/public/slide/";
+  static const String baseUrlSliderImage="https://cmi.edu.pk/qca/public/slide/";
 }
